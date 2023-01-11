b4326bed4b3251e18a5c92208ebd9a9647db29f3
Nathan Augusto <nathanaugusto1902@gmail.com>
2023-01-11 00:26:47 -0300

    Conhecimento add

--- index.php
+++ index.php
@@ -1,6 +1,23 @@
 <?php
 
-echo "Hello World";
-echo "My First Project";
+
+/*
+
+$nome = "Nathan"; // Cria uma variavel
+echo $variavel; // Chama a variavel e apresenta na tela
+unset($variavel); //Remove os Dados da variavel - Ela fica sem nada como se não existise
+echo "<br/>"; // Pula uma linha 
+exit; // Não le nada abaixo dele
+
+Salvar Git
+
+git add --all
+git commit -m "Comentario"
+git remote add origin https://github.com/NathanAugusto1902/MyStudies.git
+git push origin master
+
+git pull 
+
+*/
 
 ?>
